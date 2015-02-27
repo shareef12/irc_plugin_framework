731cdedc10b47282171526ccaf222e1a73542428
shareef12 <shareef12@twelvetacos.com>
2015-02-27 11:14:16 -0500

    Moved configuration details to a separate file

---
+++ config.h
@@ -0,0 +1,16 @@
+#ifndef _bot_h
+#define _bot_h
+
+#define SERVER      "server.net"
+#define PORT        "6697"
+#define SSL_CONN    1
+#define NICK        "cbot"
+#define NICKPASS    "pass"
+#define USER        "shareef12"
+#define REALNAME    "shareef12"
+
+#define CHANNELS    "#test"
+#define HELLO_MSG   "hello"
+#define MAINTAINER  "shareef12"
+
+#endif
